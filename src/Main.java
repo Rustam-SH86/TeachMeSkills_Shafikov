8be0dce66f4039cdad21af637ab21dc2c4a1a470
rustam20.04 <rust-shafik@yandex.ru>
2024-09-26 21:05:28 +0400

    HomeTask_10 Solved in ProcessingOfString package

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,26 @@
 
+import CreditCard.CreditCard;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[]{1,2,3};
 
-        System.out.println("Enter the initial amount for the first card:" );
-        double balance1 = getInitialBalance(scanner);
-        CreditCard card1 = new CreditCard("111-222", balance1);
+        try {
+            System.out.println("access for third element " + arr[5]);
 
+        } catch (ArrayIndexOutOfBoundsException d){
+            System.out.println("Error: " + d);
 
-        System.out.println("Enter the initial amount for the second card:");
-        double balance2 = getInitialBalance(scanner);
-        CreditCard card2 = new CreditCard("222-333", balance2);
-
-        System.out.println("Enter the initial amount for the third card:");
-        double balance3 = getInitialBalance(scanner);
-        CreditCard card3 = new CreditCard("333-444", balance3);
-
-        System.out.println("Deposit first card: ");
-        card1.depositMoney(scanner.nextInt());
-
-        System.out.println("Deposit second card: ");
-        card2.depositMoney(scanner.nextInt());
-
-
-        System.out.println("withdraw third card: ");
-        card3.withdrawMoney(scanner.nextInt());
-
-
-        System.out.println("Current carts ballance: ");
-        card1.printInfo();
-        card2.printInfo();
-        card3.printInfo();
-
-    }
-    private static double getInitialBalance(Scanner scanner) {
-        double balance = -1;
-        while (balance < 0) {
-            System.out.print("Please enter a positive number: ");
-            balance = scanner.nextDouble();
-            if (balance < 0) {
-                System.out.println("balance cannot be less than 0 ");
-            }
         }
-        return balance;
+        finally {
+            System.out.println("access for third element " + arr[2]);
+        }
+
+
     }
 }
